0f69dd9e3dcbee7c5c56d8dfa24fbca26f8a676d
dreckTrinity <dreck@trinity.edu>
2025-05-05 17:03:12 -0500

    problem one might be done

--- visualization.py
+++ visualization.py
@@ -12,20 +12,25 @@ df = pd.read_csv('train.csv', parse_dates=[2], nrows=500000, encoding='utf-16')
 # print the first 5 rows of the dataset
 # ** YOUR CODE HERE **
 
-#print(df.head())
+print(df.head())
 
 # display histogram for fares and passenger count
 # ** YOUR CODE HERE **
 
-#x = df.hist(column='passenger_count')
-#plt.show()
+x = df.hist(column='passenger_count')
+plt.show()
 
 # statistical summary of dataset
 # ** YOUR CODE HERE **
 
-#df.describe()
 
+print(df.describe())
 
 # print the number of missing values for each variable
 # ** YOUR CODE HERE **
+
+for col in df.columns:
+    n_nan = df[col].isna().sum()
+    n_zero = df[col].eq(0).sum() if pd.api.types.is_numeric_dtype(df[col]) else "N/A"
+    print(f"{col}: NaN={n_nan}, Zero={n_zero}")
 
