abfa9fc50a10a0f3707e6a3f42d5b34e02f308ba
dreckTrinity <dreck@trinity.edu>
2025-05-08 13:55:14 -0500

    results.txt updated, fixed layer #, different epoch #

--- dennis_test.bat
+++ dennis_test.bat
@@ -2,19 +2,20 @@ setlocal EnableDelayedExpansion
 REM filepath: c:\Users\denni\OneDrive\Desktop\TaxiFares\dennis_test.bat
 
 
-SET EPOCH=10
+SET EPOCH=5
 SET H1=256
 SET H2=144
 SET H3=32
-SET TR=0.1
+SET H4=16
+SET TR=0.2
 
-FOR /L %%i IN (1,1,10) DO (
+FOR /L %%i IN (1,1,20) DO (
     echo Running iteration %%i
     
- 
-    for /f "usebackq delims=" %%A in (`powershell -Command "[math]::Round(%%i * %TR%, 2)"`) do (
-        set TRTMP=%%A
-    )
-
-    c:\Users\denni\OneDrive\Desktop\TaxiFares\taxi_fare_venv\Scripts\python.exe c:\Users\denni\OneDrive\Desktop\TaxiFares\main.py -e %EPOCH% -h1 %H1% -h2 %H2% -h3 %H3% -t !TRTMP!
+    set /a H1TMP=H1 * %%i
+    set /a H2TMP=H2 * %%i
+    set /a H3TMP=H3 * %%i
+    set /a H4TMP=H4 * %%i
+    
+    c:\Users\denni\OneDrive\Desktop\TaxiFares\taxi_fare_venv\Scripts\python.exe c:\Users\denni\OneDrive\Desktop\TaxiFares\main.py -e %EPOCH% -h1 !H1TMP! -h2 !H2TMP! -h3 !H3TMP! -h4 !H4TMP! -t .2
 )
