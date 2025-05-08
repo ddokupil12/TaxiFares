333a689ab99da2168c6544621c3bc9a6ab58d9be
dreckTrinity <dreck@trinity.edu>
2025-05-08 12:41:35 -0500

    results.txt updated

--- dennis_test.bat
+++ dennis_test.bat
@@ -3,9 +3,10 @@ REM filepath: c:\Users\denni\OneDrive\Desktop\TaxiFares\dennis_test.bat
 
 
 SET EPOCH=10
-SET H1=10
-SET H2=10
-SET H3=10
+SET H1=128
+SET H2=72
+SET H3=16
+SET TR=.1
 
 
 FOR /L %%i IN (1,1,20) DO (
@@ -14,6 +15,7 @@ FOR /L %%i IN (1,1,20) DO (
     set /a H1TMP=H1 * %%i
     set /a H2TMP=H2 * %%i
     set /a H3TMP=H3 * %%i
+    set /a TRTMP=TR * %%i
 
-    c:\Users\denni\OneDrive\Desktop\TaxiFares\taxi_fare_venv\Scripts\python.exe c:\Users\denni\OneDrive\Desktop\TaxiFares\main.py -e %EPOCH% -h1 !H1TMP! -h2 !H2TMP! -h3 !H3TMP! -t .33
+    c:\Users\denni\OneDrive\Desktop\TaxiFares\taxi_fare_venv\Scripts\python.exe c:\Users\denni\OneDrive\Desktop\TaxiFares\main.py -e %EPOCH% -h1 !H1TMP! -h2 !H2TMP! -h3 !H3TMP! -t !TRTMP!
 )
